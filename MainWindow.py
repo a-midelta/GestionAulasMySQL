0e937a75f070c393b4a9a439987f29d4b0adc747
alex-midelta <alex.midelta@gmail.com>
2023-10-17 21:16:49 -0700

    Filtrar y actualizar

--- MainWindow.py
+++ MainWindow.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import (
     QMainWindow,
     QWidget,
     QVBoxLayout,
+    QHBoxLayout,
     QLabel,
     QPushButton,
     QFormLayout,
@@ -11,8 +12,9 @@ from PySide6.QtWidgets import (
     QCalendarWidget,
     QTableWidget,
     QTableWidgetItem,
+    QHeaderView,
 )
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QSize, QDate
 from MongoDBHandler import MongoDBHandler
 
 class MainWindow(QMainWindow):
@@ -28,6 +30,8 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)
 
         self.layout = QVBoxLayout(self.central_widget)
+        self.h_layout = QHBoxLayout()
+        self.filters_v_layout = QFormLayout()
         
         self.label = QLabel("Gestión de Disponibilidad de Aulas")
         self.label.setAlignment(Qt.AlignCenter)
@@ -56,16 +60,63 @@ class MainWindow(QMainWindow):
 
         self.form_layout.addRow("", QPushButton("Guardar", clicked=self.save_data))
         self.layout.addLayout(self.form_layout)
-        
+
+        self.clear_button = QPushButton("Vaciar", clicked=self.clear_form)  # Botón para vaciar el formulario
+        self.form_layout.addRow("", self.clear_button)
+
+        # Botóm para actualizar registros
+        self.update_button = QPushButton("Actualizar Registro Seleccionado", clicked=self.update_selected_row)
+        self.layout.addWidget(self.update_button)
+
         # Botón para eliminar registros
         self.delete_button = QPushButton("Eliminar Registro Seleccionado", clicked=self.delete_data)
         self.layout.addWidget(self.delete_button)
+
         
         # Tabla de Datos
         self.table = QTableWidget(self)
         self.table.setColumnCount(5)
         self.table.setHorizontalHeaderLabels(["Día", "Hora", "Nombre", "Materia", "Aula"])
-        self.layout.addWidget(self.table)
+        # Estirar encabezados horizontales de modo que usen todo el espacio
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        # Modo de selección para seleccionar filas enteras
+        self.table.setSelectionBehavior(QTableWidget.SelectRows)
+        self.table.setSelectionMode(QTableWidget.SingleSelection)
+        # Cargar datos de fila seleccionada
+        self.table.cellClicked.connect(self.load_data_from_selected_row)
+        
+        # Filtros
+        self.filters_label = QLabel("Filtros")
+        self.filters_label.setAlignment(Qt.AlignCenter)
+        self.filters_v_layout.addWidget(self.filters_label)
+        # Filtro aula
+        self.filters_classroom_combobox = QComboBox()
+        self.filters_classroom_combobox.addItems(["Todas", "Sala de capacitación A", "Sala de capacitación B"])
+        self.filters_v_layout.addRow("Aula:", self.filters_classroom_combobox)
+        self.filters_classroom_combobox.currentIndexChanged.connect(self.filter_table)
+        # Filtro nombre
+        self.filters_name_lineedit = QLineEdit()
+        self.filters_v_layout.addRow("Nombre:", self.filters_name_lineedit)
+        self.filters_name_lineedit.textChanged.connect(self.filter_table)  # Connect to filter function
+        # Filtro hora
+        self.filters_time_combobox = QComboBox()
+        self.filters_time_combobox.addItems(["Cualquiera"])
+        for hour in range(7, 21):
+            self.filters_time_combobox.addItem(f"{hour}:00")
+        self.filters_v_layout.addRow("Hora:", self.filters_time_combobox)
+        self.filters_time_combobox.currentIndexChanged.connect(self.filter_table)
+
+        # Espaciador equis
+        spacer = QWidget()
+        spacer.setFixedSize(QSize(10, 10))  # Adjust the size as needed
+        self.layout.addWidget(spacer)
+
+        # Finalizar layout
+        self.h_layout.addLayout(self.filters_v_layout, 1)
+        self.h_layout.addWidget(self.table, 7)
+        self.layout.addLayout(self.h_layout)
+        # self.layout.addWidget(self.table)
+
 
         # Cargar datos al iniciar
         self.load_data()
@@ -84,7 +135,7 @@ class MainWindow(QMainWindow):
             return
 
         if self.mongo_handler.check_duplicate(day, time, classroom):
-            print("Registro duplicado.")
+            print("Registro duplicado (día, hora y/o aula ya ocupados).")
             return
 
         # Guardar en la base de datos
@@ -125,4 +176,89 @@ class MainWindow(QMainWindow):
 
         # Actualizar la tabla
         self.load_data()
+
+    def filter_table(self):
+        aula_seleccionada = self.filters_classroom_combobox.currentText()
+        nombre_tecleado = self.filters_name_lineedit.text()
+        hora_seleccionada = self.filters_time_combobox.currentText()
+        if aula_seleccionada == "Todas":
+            # No filtrar por aula, solo nombre y hora
+            self.load_filtered_data(classroom=None, name=nombre_tecleado, time=hora_seleccionada)  
+        else:
+            # Filtrar por aula, nombre y hora
+            self.load_filtered_data(classroom=aula_seleccionada, name=nombre_tecleado, time=hora_seleccionada) 
+
+    def load_filtered_data(self, classroom=None, name=None, time=None):
+        # Limpiar tabla
+        self.table.setRowCount(0)
+
+        # Obtener datos filtrados desde la base de datos
+        filtered_data = self.mongo_handler.get_all_data(classroom, name, time)
+
+        # Llenar la tabla con los datos ya filtrados
+        for row, record in enumerate(filtered_data):
+            self.table.insertRow(row)
+            for col, value in enumerate(record.values()):
+                item = QTableWidgetItem(str(value))
+                self.table.setItem(row, col, item)
         
+    def load_data_from_selected_row(self, row, col):
+        # Obtener datos de la fila seleccionada
+        day = self.table.item(row, 0).text()
+        time = self.table.item(row, 1).text()
+        name = self.table.item(row, 2).text()
+        subject = self.table.item(row, 3).text()
+        classroom = self.table.item(row, 4).text()
+
+        # Ponerle a los widgets del formulario los datos correspondientes
+        self.day_calendar.setSelectedDate(self.day_calendar.selectedDate().fromString(day, "yyyy-MM-dd"))
+        index = self.time_combobox.findText(time, Qt.MatchExactly)
+        if index >= 0:
+            self.time_combobox.setCurrentIndex(index)
+        self.name_lineedit.setText(name)
+        self.subject_lineedit.setText(subject)
+        index = self.classroom_combobox.findText(classroom, Qt.MatchExactly)
+        if index >= 0:
+            self.classroom_combobox.setCurrentIndex(index)
+
+    def clear_form(self):
+        # Vaciar todos los campos del formulario
+        self.day_calendar.setSelectedDate(QDate.currentDate())
+        self.time_combobox.setCurrentIndex(0)
+        self.name_lineedit.clear()
+        self.subject_lineedit.clear()
+        self.classroom_combobox.setCurrentIndex(0)
+
+        # Deseleccionar selección de la tabla
+        self.table.clearSelection()
+        self.table.setCurrentItem(None)
+
+    # Function to update the selected row with form data
+    def update_selected_row(self):
+        # Check if a row is selected
+        selected_row = self.table.currentRow()
+        if selected_row == -1:
+            print("Seleccione una fila para actualizar.")
+            return
+
+        # Get the updated data from the form
+        day = self.day_calendar.selectedDate().toString("yyyy-MM-dd")
+        time = self.time_combobox.currentText()
+        name = self.name_lineedit.text()
+        subject = self.subject_lineedit.text()
+        classroom = self.classroom_combobox.currentText()
+
+        # Update the selected row in the table with the new data
+        self.table.item(selected_row, 0).setText(day)
+        self.table.item(selected_row, 1).setText(time)
+        self.table.item(selected_row, 2).setText(name)
+        self.table.item(selected_row, 3).setText(subject)
+        self.table.item(selected_row, 4).setText(classroom)
+
+        # Update the data in the database
+        self.mongo_handler.update_data(day, time, name, subject, classroom)
+
+        print("Registro actualizado correctamente.")
+
+
+
--- MongoDBHandler.py
+++ MongoDBHandler.py
@@ -15,11 +15,33 @@ class MongoDBHandler:
         count = self.collection.count_documents({"day": day, "time": time, "classroom": classroom})
         return count > 0
 
-    def get_all_data(self):
-        cursor = self.collection.find({}, {"_id": 0})
+    # def get_all_data(self):
+    #     cursor = self.collection.find({}, {"_id": 0})
+    #     data = list(cursor)
+    #     return data
+
+    def get_all_data(self, aula=None, name=None, time=None):
+        filter_query = {}  # Iniciar con un filtro vacío
+
+        if aula and aula != "Todas":
+            filter_query["classroom"] = aula  # Filtrar por aula
+
+        if name:
+            # Utilizar expresiones regulares para buscar por nombre (case-insensitive)
+            filter_query["name"] = {"$regex": name, "$options": "i"}
+
+        if time and time != "Cualquiera":
+            filter_query["time"] = time # Filtrar por hora
+
+        cursor = self.collection.find(filter_query, {"_id": 0})
         data = list(cursor)
         return data
 
     def delete_data(self, day, time):
         self.collection.delete_one({"day": day, "time": time})
+
+    def update_data(self, day, time, name, subject, classroom):
+        filter_query = {"day": day, "time": time}
+        update_data = {"$set": {"name": name, "subject": subject, "classroom": classroom}}
+        self.collection.update_one(filter_query, update_data)
         
